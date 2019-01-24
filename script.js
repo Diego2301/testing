d71d27d8f1959d00b671957437bb9a1de069c233
mercedes <mechgalera@gmail.com>
2019-01-24 15:56:02 +0100

    corrección de errores

---
+++ script.js
@@ -0,0 +1,13 @@
+.title{
+    font-size: 40px;
+   display: flex;
+   justify-items: center;
+   align-content: center;
+   text-align: center;
+}
+
+.text{
+    font-size: 20px;
+    text-justify: center;
+
+}
